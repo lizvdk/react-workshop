012b378eecdb51937ec537db92675367081caa06
Liz Vanderkloot <liz@barbershoplabs.com>
2015-04-02 20:14:58 -0400

    Nest tasks under their categories

--- task-manager/client/js/components/TaskList.react.js
+++ task-manager/client/js/components/TaskList.react.js
@@ -1,8 +1,35 @@
+var CategoryListItem = React.createClass({
+  render: function() {
+      var taskList = this.props.tasks.map((task, i) => {
+          return (
+              <TaskListItem task={task} />
+          );
+      });
+
+      return (
+          <li>{this.props.category.get('name')}
+            <ul>
+                {taskList}
+            </ul>
+          </li>
+      );
+  }
+});
+
+var TaskListItem = React.createClass({
+  render: function() {
+      return (
+          <li>{this.props.task.get('name')}</li>
+      );
+  }
+});
+
 var TaskList = React.createClass({
     render: function() {
         var categoryList = this.props.categories.map((category, i) => {
+            var tasks = this.props.tasks.where({category: category.get('id')});
             return (
-                <li key={i}>{category.get('name')}</li>
+                <CategoryListItem category={category} tasks={tasks} />
             );
         });
 
